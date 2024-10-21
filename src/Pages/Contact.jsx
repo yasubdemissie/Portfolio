f9e046d23c7b09970abe8d04600e27c83a89b72b
yasubdemissie <yasdam777@gmail.com>
2024-10-21 10:57:40 +0300

    The responsiveness of the web has finished

--- src/Pages/Contact.jsx
+++ src/Pages/Contact.jsx
@@ -5,7 +5,7 @@ import Form from "../Components/Form";
 function Contact() {
   return (
     <>
-      <div className="flex justify-start gap-y-10 overflow-x-hidden w-dvw lg:gap-4 mt-16">
+      <div className="flex flex-wrap justify-start gap-y-10 overflow-x-hidden w-dvw lg:gap-4 mt-16">
         <ContactDetail />
         <Form />
       </div>
