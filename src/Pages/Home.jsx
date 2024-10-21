986bb1fa08995ad1a66bbeedc299427a51ac8f4a
yasubdemissie <yasdam777@gmail.com>
2024-10-21 11:28:48 +0300

    document land mark

--- src/Pages/Home.jsx
+++ src/Pages/Home.jsx
@@ -4,10 +4,10 @@ import HomeImage from "../Components/HomeImage";
 
 function Home() {
   return (
-    <div className="min-h-dvh flex flex-wrap w-fit lg:mx-auto pt-10 justify-center items-center">
+    <main className="min-h-dvh flex flex-wrap w-fit lg:mx-auto pt-10 justify-center items-center">
       <HomeImage />
       <HomeDescription />
-    </div>
+    </main>
   );
 }
 
